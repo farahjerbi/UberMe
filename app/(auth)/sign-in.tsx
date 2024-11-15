27b8708b09afaae31730ae9fdfedb3a4cf37d226
farahjerbi <farah.jerbi@esprit.tn>
2024-11-15 12:41:02 +0100

    Welcome interface

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignIn=()=>{
+return(
+    <SafeAreaView>
+        <Text>
+            signin
+        </Text>
+    </SafeAreaView>
+)
+}
+export default SignIn;
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUp=()=>{
+return(
+    <SafeAreaView>
+        <Text>
+            signup
+        </Text>
+    </SafeAreaView>
+)
+}
+export default SignUp;
